3b93cbc233012349c085da98c28dba2eb1f5b3fc
Prince Asare <132716728+photonkay@users.noreply.github.com>
2024-10-10 13:48:39 +0000

    Create task_3.cpp

---
+++ session_03_assignment/task_3.cpp
@@ -0,0 +1,19 @@
+// The code is supposed to check if a number is even or odd
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    
+    // Check for even or odd (logical error)
+    if (num % 2 = 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    
+    return 0;
+}
