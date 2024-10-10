5efcfa21f60899885ac697a12d39fab16c1ab3f5
Prince Asare <132716728+photonkay@users.noreply.github.com>
2024-10-10 13:54:52 +0000

    Update task_2.cpp

--- session_03_assignment/task_2.cpp
+++ session_03_assignment/task_2.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main() {
     int i = 1;
     
-    // Loop to print numbers from 1 to 5 (logical error causes infinite loop)
     while (i <= 5) {
         cout << i << endl;
     }
